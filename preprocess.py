92da4c4d63fb7b640f05649eb959611337935cbf
Hayden Chiu <haydenchiush@gmail.com>
2024-05-16 18:12:49 -0700

    added sentence embedding k-means model

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import msoffcrypto
 import io
 
-def read_encrypted_data(passwd = 'Capstone_2024', path_to_data = "data/CONFIDENTIAL_SampleData_Shared-2024-05-08.xlsx"):
+def read_encrypted_data(passwd = 'Capstone_2024', path_to_data = "data/CONFIDENTIAL_2024UBC_X99_3-Month-DataSet-REDACTED-Shared 2024-05-15.xlsx"):
     decrypted_workbook = io.BytesIO()
     with open(path_to_data, 'rb') as file:
         office_file = msoffcrypto.OfficeFile(file)
@@ -19,6 +19,7 @@ def reformat_data(data):
 
     agg_funcs = {
         col: 'first' for col in ["Grouped_Event_Type_Code_Desc", "Grouped_Event_Subtype_Code_Desc", 
+                                 "Occurrence_Type", "Occurrence_Type_UCR_Category", "Occurrence_Report_Category",
                                  "Priority", "Public_Generated_Event_Flag", "Event_Attended_Flag"]
     }
     agg_funcs["Event_Remarks_Text"] = lambda x: "\n".join(x)
